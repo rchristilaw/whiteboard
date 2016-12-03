57bc2e63e0ddb8a3bd4db79d5a2c0ae32048e10f
rchristilaw <ryan.christilaw@gmail.com>
2016-12-03 12:38:25 -0500

    Restructure and get basic angular component working

---
+++ app/scripts/placeholder-page.js
@@ -0,0 +1,3 @@
+angular.module('whiteboardapp').component('placeholderPage', {
+  templateUrl: 'scripts/placeholder-page.html'
+});
--- scripts/placeholder-page.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module("whiteboardApp", []);
-
-app.directive("placeholderPage", function() {
-    return {
-        templateUrl : "placeholder-page.html"
-    };
-});
